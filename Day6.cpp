ee1063aa96af676d683a95ac7eaaab8c8350d987
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-07-19 20:47:54 +0000

    day6donee

--- Day6.cpp
+++ Day6.cpp
@@ -51,7 +51,24 @@ public:
 };
 
 
-Q3. 
+Q3. 12. Integer to Roman
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string roman;
+        vector<string> notations = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        for(int i = 0; num>0; i++){
+            while(num >= values[i]){
+                roman += notations[i];
+                num -= values[i];
+            }
+        }
+        return roman;
+    }
+};   Time Complexity (TC): O(1), Space Complexity (SC): O(logn)
 
 
 Q4. 22. Generate Parentheses
